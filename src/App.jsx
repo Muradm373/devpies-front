61f379b803d7a853cfb41d45b52f33fb23607796
muradm373 <muradm373@gmail.com>
2021-01-13 01:26:07 +0400

    Main components pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
