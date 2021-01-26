999aa90234d84fed4f3c126dd40cf2d1e100e7a7
muradm373 <muradm373@gmail.com>
2021-01-26 15:13:57 +0400

    Interactive 3d model

--- src/components/common/BodyScene.jsx
+++ src/components/common/BodyScene.jsx
@@ -1,19 +1,34 @@
 import React, { Component, useRef, Suspense } from "react";
 import { Canvas } from "react-three-fiber";
-import { useGLTF, OrbitControls, ContactShadows } from "drei";
+import { useGLTF, OrbitControls, ContactShadows, Environment } from "drei";
 import Body from "./Body";
 
 export default function BodyScene(props) {
   return (
     <div style={{ height: "60vh" }}>
-      <Canvas>
+      <Canvas concurrent pixelRatio={[1, 2]}>
         <ambientLight intensity={0.5} />
-        <spotLight intensity={0.3} position={[5, 30, 30]} />
-        <spotLight intensity={0.3} position={[-5, -30, -30]} />
+        <spotLight
+          intensity={0.3}
+          position={[5, 30, 30]}
+          angle={0.1}
+          penumbra={1}
+        />
+        <spotLight
+          intensity={0.3}
+          position={[-5, -30, -30]}
+          angle={0.1}
+          penumbra={1}
+        />
         <Suspense fallback={null}>
           <Body />
+          {/* <Environment files="hdr.hdr" /> */}
         </Suspense>
-        <OrbitControls />
+        <OrbitControls
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+          enablePan={false}
+        />
       </Canvas>
     </div>
   );
